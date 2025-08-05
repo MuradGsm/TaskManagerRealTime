441253f35002ad74dff3cabee4bcb5aba350d48d
Murad Seferov <45018603-seferovmurad98@users.noreply.replit.com>
2025-08-05 13:31:14 +0000

    add_pydantic_models

---
+++ models/user.py
@@ -0,0 +1,30 @@
+from pydantic import BaseModel, Field
+from datetime import datetime
+
+
+class UserRequest(BaseModel):
+    username: str
+    email: str
+    full_name: str
+    password: str
+    is_active: bool
+    is_admin: bool = Field(default=False)
+    status: str
+    avatar_url: str
+
+
+class UserResponse(BaseModel):
+    id: int
+    username: str
+    email: str
+    full_name: str
+    hashed_password: str
+    is_active: bool
+    is_admin: bool = Field(default=False)
+    status: str
+    avatar_url: str
+    created_at: datetime
+    last_seen: datetime
+
+    class Config:
+        orm_mode = True
